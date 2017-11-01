9702aa8cb1d710a06f6ba574ba7e5ebcde344bf6
Krishn Ramesh <krishnr@gmail.com>
2017-11-01 15:14:46 -0400

    error handling for produce.c

--- lab3/starter/tools/produce.c
+++ lab3/starter/tools/produce.c
@@ -1,7 +1,7 @@
 /**
- * @file: produce.c
+ * @file:   sender.c
  * @brief:  send random points to a POSIX queue. Type 'q' to terminate. 
- * @date:   2017/11/01
+ * @date:   2014/06/25
  * NOTES:
  * To execute: <executable> <arg1>, where arg1 is a posix queue name.
  * EXAMPLE: ./sender.out /mailbox1_userid 
@@ -9,3 +9,22 @@
  * Execute command "man mq_overview" for details.
  * Check /dev/mqueue to clean up the queue if sender fails to do so.
  */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    char  quit = '\0';
+
+    if ( argc !=5 ) {
+        printf("Usage: %s <N> <B> <P> <C> \n", argv[0]);
+        exit(1);
+    }
+    
+    return 0;
+}

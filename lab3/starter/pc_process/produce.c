b69a9fa8c074a146e14027f9920e3ed2da097e96
Krishn Ramesh <krishnr@gmail.com>
2017-11-22 19:14:19 -0500

    added comments

--- lab3/starter/pc_process/produce.c
+++ lab3/starter/pc_process/produce.c
@@ -1,6 +1,6 @@
 /**
  * @file:   produce.c
- * @brief: 
+ * @brief:  Implementing the producer-consumer paradigm with multiple processes and message queues.
  * @date:   2017/11/22
  * NOTES:
     Compile with: gcc produce.c -lrt -lm -o produce
@@ -28,22 +28,26 @@ void consumer(int id);
 
 int main(int argc, char *argv[])
 {
+    // Storing start time as t1
     struct timeval tv;
     double t1;
     double t2;
     gettimeofday(&tv, NULL);
     t1 = tv.tv_sec + tv.tv_usec/1000000.0;
 
+    // Checking that all the args are passed in
     if ( argc !=5 ) {
         printf("Usage: %s <N> <B> <P> <C> \n", argv[0]);
         exit(1);
     }
 
+    // Converting the args to ints
     N = atoi(argv[1]);
     B = atoi(argv[2]);
     P = atoi(argv[3]);
     C = atoi(argv[4]);
 
+    // Initializing the message queue attributes
     mqd_t qdes;
     struct mq_attr attr;
     attr.mq_maxmsg = B;
@@ -51,19 +55,21 @@ int main(int argc, char *argv[])
     attr.mq_flags = 0;
     attr.mq_curmsgs = 0;
 
+    // Creating the queue and setting the mode permissions to all users
     qdes = mq_open(Q_NAME, (O_CREAT | O_RDWR), (S_IRWXU | S_IRWXG | S_IRWXO), &attr);
 
+    // If queue creation failed
     if (qdes == -1) {
         perror("Main - mq_open() failed");
         return 1;
     }
 
-    // Create producers and consumers
+    // Store producer and consumer pids
     pid_t* prod_pids = malloc(P * sizeof(pid_t));
     pid_t* con_pids = malloc(C * sizeof(pid_t));
 
     int i;
-    // create P producers
+    // Create P producer processes
     for (i = 0; i < P; i++) {
         prod_pids[i] = fork();
         if (prod_pids[i] < 0) {
@@ -74,7 +80,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    // create C consumers
+    // Create C consumer processes
     for (i = 0; i < C; i++) {
         con_pids[i] = fork();
         if (con_pids[i] < 0) {
@@ -85,14 +91,13 @@ int main(int argc, char *argv[])
         }
     }
 
-    // wait for all producer and consumer threads to exit
-    // before closing the mq
+    // Wait for all producers to finish
     int* ret_val;
     for(i = 0; i < P; i++) {
         waitpid(prod_pids[i], ret_val, 0);
     }
 
-    // Terminate all consumers
+    // Send -1 kill signals to the queue to terminate all consumers
     int kill = -1;
     for (i = 0; i < C; i++) {
         if (mq_send(qdes,(char *) &kill, sizeof(int), 0) == -1) {
@@ -101,11 +106,12 @@ int main(int argc, char *argv[])
         }
     }
 
+    // Wait for all consumers to finish
     for(i = 0; i < C; i++) {
         waitpid(con_pids[i], ret_val, 0);
     }
 
-
+    // Close the queue and do cleanup
     if (mq_close(qdes) == -1) {
         perror("mq_close() failed");
         exit(2);
@@ -119,6 +125,7 @@ int main(int argc, char *argv[])
     free(prod_pids);
     free(con_pids);
 
+    // Store the finish time and print the execution time
     gettimeofday(&tv, NULL);
 
     t2 = tv.tv_sec + tv.tv_usec/1000000.0;
@@ -129,12 +136,15 @@ int main(int argc, char *argv[])
 
 void producer(int id) {
     int i;
+    
+    // Open queue for writing 
     mqd_t q = mq_open(Q_NAME, O_WRONLY);
     if (q == -1) {
         perror("Producer - mq_open() failed");
         exit(1);
     }
 
+    // Send ints to the queue according to i%P == id
     for (i = 0; i < N; i++) {
         if (i%P == id) {
             if (mq_send(q,(char *) &i, sizeof(int), 0) == -1) {
@@ -144,12 +154,14 @@ void producer(int id) {
         }
     }
 
+    // Close the queue and exit the process
     mq_close(q);
     exit(0);
 }
 
 
 void consumer(int cid) {
+    // Open the queue for reading
     mqd_t q = mq_open(Q_NAME, O_RDWR);
     if (q == -1) {
         perror("Consumer - mq_open() failed");
@@ -159,17 +171,20 @@ void consumer(int cid) {
     int pt;
     int root;
     while (true){
+        // If there's an error receiving from queue
         if (mq_receive(q, (char *) &pt, sizeof(int), NULL) == -1) {
             perror("mq_receive() failed");
             printf("Consumer: %d failed.\n", cid);
             exit(1);
         } else {
+            // Else the consumer read the point correctly
             
-            // Encounters kill signal
+            // Encounters kill signal so break out of loop
             if (pt < 0) {
                 break;
             }
 
+            // Calculate and print sqroot
             root = sqrt(pt);
             if (root*root == pt) {
                 printf("%d %d %d\n", cid, pt, root);
@@ -177,6 +192,7 @@ void consumer(int cid) {
         }
     }
 
+    // Close the queue and exit the process
     mq_close(q);
     exit(0);
 }
--- lab3/starter/pc_thread/produce.c
+++ lab3/starter/pc_thread/produce.c
@@ -1,6 +1,6 @@
 /**
  * @file:   produce.c
- * @brief: 
+ * @brief:  Implementing the producer-consumer paradigm with multiple threads and shared memory.
  * @date:   2017/11/22
  * NOTES:
     Compile with: gcc produce.c -pthread -lm -o produce
@@ -25,45 +25,56 @@ int C; // Number of consumers
 void *producer(void * arg);
 void *consumer(void * arg);
 
+// Shared circular buffer that holds the messages
 int* shared_buffer;
 
+// 2 semaphores to coordinate consumers and producers
 sem_t items;
 sem_t spaces;
+// 1 mutex to protect access to shared memory
 pthread_mutex_t mutex;
 
+// Shared indexes that keep track of where the all the producers/consumers are writing/reading
 int p_index = 0;
 int c_index = 0;
 
 int main(int argc, char *argv[])
 {
+    // Storing start time as t1
     struct timeval tv;
     double t1;
     double t2;
     gettimeofday(&tv, NULL);
     t1 = tv.tv_sec + tv.tv_usec/1000000.0;
 
+    // Checking that all the args are passed in
     if ( argc !=5 ) {
         printf("Usage: %s <N> <B> <P> <C> \n", argv[0]);
         exit(1);
     }
 
+    // Converting the args to ints
     N = atoi(argv[1]);
     B = atoi(argv[2]);
     P = atoi(argv[3]);
     C = atoi(argv[4]);
 
+    // Setting the shared buffer to hold B integers
     shared_buffer = malloc(B * sizeof(int));
 
-    // Create producers and consumers
+    // Storing thread ids of producers and consumers
     pthread_t producers[P];
     pthread_t consumers[C];
 
+    // Initializing items to 0 as no items produced to start off
     sem_init(&items, 0, 0);
+    // Initializing spaces to B as all the buffer spaces empty to start off
     sem_init(&spaces, 0, B);
+    // Initializing mutex
     pthread_mutex_init(&mutex, NULL);
 
     int i;
-    // create P producers
+    // create P producer threads
     for (i = 0; i < P; i++) {
         int* id = malloc(sizeof(int));
         *id = i;
@@ -73,7 +84,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    // create C consumers
+    // create C consumer threads
     for (i = 0; i < C; i++) {
         int* id = malloc(sizeof(int));
         *id = i;
@@ -83,7 +94,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    // join all producers and consumers
+    // Join all producers and consumers
     void* ret_val;
     for(i = 0; i < P; i++) {
         pthread_join(producers[i], &ret_val);
@@ -94,12 +105,14 @@ int main(int argc, char *argv[])
         free(ret_val);
     }
 
+    // Cleanup
     pthread_mutex_destroy(&mutex);
     sem_destroy(&items);
     sem_destroy(&spaces);
     
     free(shared_buffer);
 
+    // Store the finish time and print the execution time
     gettimeofday(&tv, NULL);
 
     t2 = tv.tv_sec + tv.tv_usec/1000000.0;
@@ -112,17 +125,22 @@ void *producer(void* arg) {
     int* pid = (int *) arg;
 
     int i;
+    // Add ints to the buffer according to i%P == pid
     for (i = 0; i < N; i++) {
         if (i%P == *pid) {
             sem_wait(&spaces);
             pthread_mutex_lock(&mutex);
+
+            // Write to p_index % B (circular buffer)
             shared_buffer[p_index % B] = i;
             p_index++;
+            
             pthread_mutex_unlock(&mutex);
             sem_post(&items);
         }
     }
     
+    // Cleanup and exit thread
     free(arg);
     pthread_exit(0);
 }
@@ -137,10 +155,11 @@ void *consumer(void* arg) {
         sem_wait(&items);
         pthread_mutex_lock(&mutex);
         
+        // Read from c_index % B (circular buffer)
         num = shared_buffer[c_index % B];
         c_index++;
 
-        // Read N items so break
+        // Already consumed N items so break
         if (c_index >= N-1) {
             break;
         }
@@ -148,15 +167,19 @@ void *consumer(void* arg) {
         pthread_mutex_unlock(&mutex);
         sem_post(&spaces);
 
+        // Calculate and print sqrt
         root = sqrt(num);
         if (root*root == num) {
             printf("%d %d %d\n", *cid, num, root);
         }
     }
+    // Thread broke out of loop because N items have been read
+    // Post semaphores and unlock mutex to let any stuck threads through
     sem_post(&spaces);
     sem_post(&items);
     pthread_mutex_unlock(&mutex);
 
+    // Cleanup and exit thread
     free(arg);
     pthread_exit(0);
 }

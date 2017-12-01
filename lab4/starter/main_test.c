655560e272566d4cc2fe5f5cd11a0479e1f4af37
rfarmaha <ramandeep.f@gmail.com>
2017-12-01 17:22:08 -0500

    Implemented Worst Fit. Need to now add test cases

--- lab4/starter/main_test.c
+++ lab4/starter/main_test.c
@@ -16,16 +16,30 @@
 #include "mem.h"
 
 int main(int argc, char *argv[]) {
-    best_fit_memory_init(1024);
-    int* p = best_fit_alloc(8);
-    int num;
-    if (p != NULL) {
-        printf("Size: %d\n", *(p-1));
-        num = best_fit_count_extfrag(8);
-	    printf("num = %d\n", num);
-        best_fit_dealloc(p);
-        num = best_fit_count_extfrag(8);
-	    printf("num = %d\n", num);
+//    best_fit_memory_init(1024);
+//    int* p = best_fit_alloc(8);
+//    int num;
+//    if (p != NULL) {
+//        printf("Size: %d\n", *(p-1));
+//        num = best_fit_count_extfrag(8);
+//	    printf("num = %d\n", num);
+//        best_fit_dealloc(p);
+//        num = best_fit_count_extfrag(8);
+//	    printf("num = %d\n", num);
+//    } else {
+//        printf("NULL pointer returned\n");
+//    }
+
+    worst_fit_memory_init(1024);
+    int* p1 = worst_fit_alloc(8);
+    int num1;
+    if (p1 != NULL) {
+        printf("Size: %d\n", *(p1-1));
+        num1 = worst_fit_count_extfrag(8);
+        printf("num = %d\n", num1);
+        worst_fit_dealloc(p1);
+        num1 = worst_fit_count_extfrag(8);
+        printf("num = %d\n", num1);
     } else {
         printf("NULL pointer returned\n");
     }
--- lab4/starter/mem.c
+++ lab4/starter/mem.c
@@ -20,6 +20,7 @@ void* b_mem_box;
 void* b_free_space;
 int* b_bitmap;
 int M;
+int N;
 
 void* w_mem_box;
 void* w_free_space;
@@ -86,7 +87,40 @@ int best_fit_memory_init(size_t size)
 
 int worst_fit_memory_init(size_t size)
 {
-	return 0;
+
+    if (size < 4) return -1;
+
+    w_mem_box = malloc(size);
+    w_bitmap = (int*) w_mem_box;
+
+    // N number of blocks
+    N = size/BLOCK_SIZE;
+
+    int n = BLOCK_SIZE * 8;
+
+    // size in bytes
+    int bitmap_size = (int) (N + (n - N%n));
+
+    printf("Bitmap size: %d, size: %zu, bitmap: %d\n", bitmap_size, size, w_bitmap);
+
+    int i;
+    for (i = 0; i < N; i++) {
+        if (i < bitmap_size/n) {
+            SetBit(w_bitmap, i);
+        } else {
+            ClearBit(w_bitmap, i);
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        printf("%d", TestBit(w_bitmap,i));
+    }
+    printf("\n");
+
+    w_free_space = w_mem_box + bitmap_size;
+    printf("bitmap address: %d, bitmap_size: %d, free_space address: %d\n", w_mem_box, bitmap_size, w_free_space);
+
+    return 0;
 }
 
 
@@ -183,7 +217,71 @@ void *best_fit_alloc(size_t size)
 void *worst_fit_alloc(size_t size)
 {
 	// To be completed by students
-	return NULL;
+    int worst_fit_start = 0, start = 0, in_contiguous = 0, contiguous_size = 0, worst_fit_size = 0;
+
+    int required_blocks = size/BLOCK_SIZE;
+    // if there is a remainder need an extra block
+    if (size % BLOCK_SIZE != 0) {
+        required_blocks++;
+    }
+
+    if (size == 0) return NULL;
+
+    // Find the worst fit start and size
+    // i.e. Find the largest contiguous set of 0s
+    int i;
+    for (i = 0; i < N; i++) {
+        printf("Bit %d: %d, in_contiguous: %d, contiguous_size: %d\n",i, TestBit(w_bitmap,i), in_contiguous, contiguous_size);
+
+        // entering a contiguous section
+        if (TestBit(w_bitmap,i) == 0 && !in_contiguous) {
+            in_contiguous = 1;
+            start = i;
+            printf("%d: entering contiguous section\n", i);
+        }
+
+        // in a contiguous section
+        if (TestBit(w_bitmap,i) == 0 && in_contiguous) {
+            contiguous_size++;
+            printf("%d: in contiguous section\n", i);
+        }
+
+        // leaving a contiguous section
+        if ((TestBit(w_bitmap,i) == 1 && in_contiguous) || i == N-1) {
+            printf("%d: left contiguous section, contiguous_size: %d, worst_fit_size: %d\n", i, contiguous_size, worst_fit_size);
+            in_contiguous = 0;
+            if (contiguous_size > worst_fit_size && contiguous_size > required_blocks) {
+                if (contiguous_size > required_blocks + 1) {
+                    contiguous_size = required_blocks + 1;
+                }
+                worst_fit_size = contiguous_size;
+                worst_fit_start = start;
+            }
+            contiguous_size = 0;
+        }
+    }
+
+    printf("worst_fit_start: %d, worst_fit_size: %d\n",worst_fit_start, worst_fit_size);
+
+    // No free space or free space is less than number of blocks required
+    if (worst_fit_size == 0) return NULL;
+
+    // Mark all the blocks as used in bitmap
+    for (i = worst_fit_start; i < worst_fit_start + worst_fit_size; i++) {
+        SetBit(w_bitmap,i);
+    }
+
+    for (i = 0; i < N; i++) {
+        printf("%d", TestBit(w_bitmap,i));
+    }
+    printf("\n");
+
+    // Mark first block of best_fit with size
+    void *temp = w_free_space + (worst_fit_start)*BLOCK_SIZE;
+    int* size_block = (int *) temp;
+    *size_block = worst_fit_size;
+
+    return w_free_space + (worst_fit_start+1)*BLOCK_SIZE;
 }
 
 /* memory de-allocator */
@@ -214,9 +312,27 @@ void best_fit_dealloc(void *ptr)
 
 void worst_fit_dealloc(void *ptr) 
 {
+    int* user_mem_start = (int*) ptr;
+    int* block_mem_start = user_mem_start - 1;
+    int block_size = *block_mem_start;
 
-	// To be completed by students
-	return;
+    printf("Dealloc size = %d\n", block_size);
+
+    int block_index = block_mem_start - (int*) w_free_space;
+    printf("Block index = %d\n", block_index);
+
+    int i;
+    // Mark all the blocks as free in bitmap
+    for (i = block_index; i < block_index + block_size; i++) {
+        ClearBit(w_bitmap,i);
+    }
+
+    for (i = 0; i < M; i++) {
+        printf("%d", TestBit(w_bitmap,i));
+    }
+    printf("\n");
+
+    return;
 }
 
 /* memory algorithm metric utility function(s) */
@@ -241,16 +357,12 @@ int best_fit_count_extfrag(size_t size)
     for (i = 0; i < M; i++) {
 
         // entering a contiguous section
-        if (TestBit(b_bitmap,i) == 0 && !in_contiguous) {
+        if (TestBit(b_bitmap,i) == 0 && !in_contiguous)
             in_contiguous = 1;
-            // printf("%d: entering contiguous section\n", i);
-        }
 
         // in a contiguous section
-        if (TestBit(b_bitmap,i) == 0 && in_contiguous) {
+        if (TestBit(b_bitmap,i) == 0 && in_contiguous)
             contiguous_size++;
-            // printf("%d: in contiguous section\n", i);
-        }
 
         // leaving a contiguous section
         if ((TestBit(b_bitmap,i) == 1 && in_contiguous) || i == M-1) {
@@ -268,6 +380,36 @@ int best_fit_count_extfrag(size_t size)
 
 int worst_fit_count_extfrag(size_t size)
 {
-	// To be completed by students
-	return 0;
+    if (size == 0) return -1;
+
+    int required_blocks = size/BLOCK_SIZE;
+    // if there is a remainder need an extra block
+    if (size % BLOCK_SIZE != 0) {
+        required_blocks++;
+    }
+    int in_contiguous = 0;
+    int contiguous_size = 0;
+    int num_fits = 0;
+
+    int i;
+    for (i = 0; i < N; i++) {
+
+        // entering a contiguous section
+        if (TestBit(w_bitmap,i) == 0 && !in_contiguous)
+            in_contiguous = 1;
+
+        // in a contiguous section
+        if (TestBit(w_bitmap,i) == 0 && in_contiguous)
+            contiguous_size++;
+
+        // leaving a contiguous section
+        if ((TestBit(w_bitmap,i) == 1 && in_contiguous) || i == N-1) {
+            in_contiguous = 0;
+            if (contiguous_size < required_blocks + 1) {
+                num_fits++;
+                contiguous_size = 0;
+            }
+        }
+    }
+    return num_fits;
 }
